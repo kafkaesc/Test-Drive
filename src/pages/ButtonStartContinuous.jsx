8e082554921a1bb7749b237c7883d8c693f567aa
Jared Hettinger <Jared.Hettinger@Jareds-MacBook-Pro.local>
2024-02-15 21:47:57 -0600

    Formating the BSC page and adding Link home

--- src/pages/ButtonStartContinuous.jsx
+++ src/pages/ButtonStartContinuous.jsx
@@ -1,11 +1,17 @@
 import H1 from '../elements/H1';
+import Link from '../elements/Link';
 import ButtonStartContinuousTour from '../tours/ButtonStartContinuousTour';
 
 export default function ButtonStartContinuous() {
 	return (
 		<div>
 			<H1 className="mb-2">Button Start into a Continuous Tour</H1>
-			<ButtonStartContinuousTour />
+			<div className="mb-2">
+				<ButtonStartContinuousTour />
+			</div>
+			<div className="text-center">
+				<Link to="/">Home</Link>
+			</div>
 		</div>
 	);
 }
