40d5a4cd32eb5a9d69afc55991cfe4eeed2bb656
Jared Hettinger <Jared.Hettinger@Jareds-MacBook-Pro.local>
2024-02-15 22:03:40 -0600

    Moving test

--- src/pages/Home.test.js
+++ src/pages/Home.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import Home from './Home';
 
 test('Renders page title', () => {
-	render(<App />);
+	render(
+		<BrowserRouter>
+			<Home />
+		</BrowserRouter>
+	);
 	const heading = screen.getByRole('heading', { level: 1 });
 	expect(heading).toBeInTheDocument();
 	expect(heading).toHaveTextContent(/test drive/i);
