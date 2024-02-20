db92e105c5b569cc8b52f85d06008013c30cda79
kafkaesc <jared.a.hettinger@gmail.com>
2024-02-19 21:46:26 -0600

    Tailwind class ordering

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Link from '../elements/Link';
 export default function Home() {
 	return (
 		<>
-			<h1 className="text-7xl font-bold text-center mb-2">Test Drive</h1>
+			<h1 className="mb-2 font-bold text-center text-7xl">Test Drive</h1>
 			<div className="text-center">
 				<Link to="/button-start-continuous">
 					Button Start into a Continuous Tour
