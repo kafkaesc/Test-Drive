2e3ac01d5aa63c578f09d646dce8a4dbaa5e8582
kafkaesc <jared.a.hettinger@gmail.com>
2024-02-19 21:39:15 -0600

    Adding bg color to UiCard and Button components

--- src/components/UiCard.jsx
+++ src/components/UiCard.jsx
@@ -1,9 +1,11 @@
 export default function UiCard({ children, className }) {
-	return className ? (
-		<div className={`inline-block w-full p-1 border border-2 ${className}`}>
+	return (
+		<div
+			className={`inline-block w-full p-1 bg-white border border-2${
+				className ? ' ' + className : ''
+			}`}
+		>
 			{children}
 		</div>
-	) : (
-		<div className="inline-block w-full p-1 border border-2">{children}</div>
 	);
 }
--- src/elements/Button.jsx
+++ src/elements/Button.jsx
@@ -1,7 +1,7 @@
 export default function Button({ children, className, size, ...props }) {
 	return size === 'large' ? (
 		<button
-			className={`px-3 py-1.5 border rounded-sm border-indigo-400 hover:bg-indigo-300${
+			className={`px-3 py-1.5 border rounded-sm border-indigo-400 bg-white hover:bg-indigo-300${
 				className ? ' ' + className : ''
 			}`}
 			{...props}
@@ -10,7 +10,7 @@ export default function Button({ children, className, size, ...props }) {
 		</button>
 	) : size === 'small' ? (
 		<button
-			className={`px-1 py-0.5 border rounded-sm border-indigo-400 hover:bg-indigo-300${
+			className={`px-1 py-0.5 border rounded-sm border-indigo-400 bg-white hover:bg-indigo-300${
 				className ? ' ' + className : ''
 			}`}
 			{...props}
@@ -19,7 +19,7 @@ export default function Button({ children, className, size, ...props }) {
 		</button>
 	) : (
 		<button
-			className={`px-2 py-1 border rounded-sm border-indigo-400 hover:bg-indigo-300${
+			className={`px-2 py-1 border rounded-sm border-indigo-400 bg-white hover:bg-indigo-300${
 				className ? ' ' + className : ''
 			}`}
 			{...props}
